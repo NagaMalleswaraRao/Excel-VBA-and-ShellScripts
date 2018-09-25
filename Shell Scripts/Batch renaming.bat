6e6fde2bbf2b998042f52676a3c16a2f2a509800
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2018-09-25 16:46:50 +0530

    Update Batch renaming.bat

--- Shell Scripts/Batch renaming.bat
+++ Shell Scripts/Batch renaming.bat
@@ -15,3 +15,6 @@ cls
 
 ::Execute the renaming operation:
 Get-ChildItem -Filter "*0118*" -Recurse | Rename-Item -NewName {$_.name -replace '0118','0218' } 
+
+:: If you want to append a string ("_AP Units") to all the files in a directory
+Get-ChildItem | Rename-Item -NewName {$_.basename + "_AP Units" + $_.extension}
