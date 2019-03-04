c3702c352869033b4eefb567b7644d76331dc2a1
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2019-03-04 17:52:03 +0530

    Update StartupRoutine.bat

--- Shell Scripts/StartupRoutine.bat
+++ Shell Scripts/StartupRoutine.bat
@@ -6,27 +6,23 @@
 :: Call VB Script (named StartCiscoVPN) 
 Start C:\Public\"Batch Files"\StartCiscoVPN.vbs
 
-:: Wait a few seconds, it would look cool if every application starts one after another and you can see the process
+:: Wait a few seconds (It would look cool if every application starts one after another and you are able see the process)
 timeout /t 6
 
 :: Open Outlook application
 Start C:\"Program Files (x86)\Microsoft Office"\root\Office16\OUTLOOK.EXE
 
+:: Wait and Start Skype for business
 timeout /t 5
-
-:: Start Skype for business
 start C:\"Program Files (x86)\Microsoft Office"\root\Office16\lync.exe
 
+:: Wait and Open a file explorer window
 timeout /t 3
-
-:: Open a file explorer window
 START C:\Windows\explorer.EXE
 
+:: Wait and Open Personal emails, these have passwords saved. So, you can get away with not entering credentials
 timeout /t 4
-
-:: Open Personal emails, these have passwords saved. So, you can get away with not entering credentials
 start chrome https://mail.google.com/mail/u/0/#inbox
-
 start chrome https://mail.google.com/mail/u/1/#inbox
 
 Exit
